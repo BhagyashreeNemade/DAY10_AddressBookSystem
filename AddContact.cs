8e78e9ce87f539e57b92e66e5f8a02843653d3e2
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-06 18:48:24 +0530

    Add a new Contact to address book

---
+++ AddContact.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddContact
+    {
+        public List<CreateContact> contactlist = new List<CreateContact>();
+        public void AddNewContact()
+        {
+            CreateContact contact = new CreateContact();
+
+            Console.WriteLine("enter first name");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("enter second name");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("enter address name");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("enter phno name");
+            contact.PhNo = Console.ReadLine();
+
+            Console.WriteLine("enter city name");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("enter state name");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("enter zip name");
+            contact.Zip = Console.ReadLine();
+
+            Console.WriteLine("enter Email name");
+            contact.Email = Console.ReadLine();
+
+            contactlist.Add(contact);
+
+        }
+        public void Display()
+        {
+            foreach (CreateContact contact in contactlist)
+            {
+                Console.WriteLine(contact.FirstName);
+                Console.WriteLine(contact.LastName);
+                Console.WriteLine(contact.Address);
+                Console.WriteLine(contact.City);
+                Console.WriteLine(contact.State);
+                Console.WriteLine(contact.PhNo);
+                Console.WriteLine(contact.Zip);
+                Console.WriteLine(contact.Email);
+
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook");
-            CreateContact contact = new CreateContact();
+            //CreateContact contact = new CreateContact();
+            AddContact addcontact = new AddContact();
+            addcontact.AddNewContact();
+            Console.WriteLine();
+            addcontact.Display();
 
         }
     }
