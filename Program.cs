462a4d6f47c38dcfd5d15843ab263108d6ed39cf
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-06 19:10:20 +0530

    edit contact person using their name

--- Program.cs
+++ Program.cs
@@ -6,14 +6,30 @@
         {
             Console.WriteLine("Welcome to AddressBook");
             //CreateContact contact = new CreateContact();
-            AddContact addcontact = new AddContact();
-            addcontact.AddNewContact();
+            //AddContact addcontact = new AddContact();
+            //addcontact.AddNewContact();
+            //Console.WriteLine();
+            //addcontact.Display();
+            //Console.WriteLine();
+            //addcontact.EditContact();
+            //Console.WriteLine();
+            //addcontact.Display();
+            //editContact editcontact = new editContact();
+            //editcontact.AddNewContact();
+            //Console.WriteLine();
+            //editcontact.Display();
+            //Console.WriteLine();
+            //editcontact.EditContact();
+            //Console.WriteLine();
+            //editcontact.Display();
+            deleteContact deletecontact = new deleteContact();
+            deletecontact.AddNewContact();
             Console.WriteLine();
-            addcontact.Display();
+            deletecontact.Display();
             Console.WriteLine();
-            addcontact.EditContact();
+            deletecontact.RemoveContact();
             Console.WriteLine();
-            addcontact.Display();
+            deletecontact.Display();
 
         }
     }
