77c2a27abfd5025174c50e32e8173c2ccc28519d
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-08 00:35:56 +0530

    address Book has a Unique Name

---
+++ AddressBook.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBook
+    {
+        public List<CreateContact> addresslist = new List<CreateContact>();
+        Dictionary<string, List<CreateContact>> dict = new Dictionary<string, List<CreateContact>>();
+        public void Addcontact(CreateContact contact)
+        {
+            addresslist.Add(contact);
+        }
+        public void Editcontact(string name)
+        {
+            foreach (var contact in addresslist)
+            {
+                if (contact.FirstName == name || contact.LastName == name)
+                {
+                    bool flag = true;
+                    while (flag == true)
+                    {
+                        Console.WriteLine("what to be edited:\n1.firstname\n2.lastname\n3.address\n4.city\n5.state\n6.zip\n7.phone.no\n8.email-id\n9.exit");
+                        int option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                string Firstname = Console.ReadLine();
+                                contact.FirstName = Firstname;
+                                Display();
+                                break;
+                            case 2:
+                                string Lastname = Console.ReadLine();
+                                contact.LastName = Lastname;
+                                Display();
+                                break;
+                            case 3:
+                                string address = Console.ReadLine();
+                                contact.Address = address;
+                                Display();
+                                break;
+                            case 4:
+                                string City = Console.ReadLine();
+                                contact.City = City;
+                                Display();
+                                break;
+                            case 5:
+                                string State = Console.ReadLine();
+                                contact.State = State;
+                                Display();
+                                break;
+                            case 6:
+                                string Zip = Console.ReadLine();
+                                contact.Zip = Zip;
+                                Display();
+                                break;
+                            case 7:
+                                string PhoneNo = Console.ReadLine();
+                                contact.PhNo = PhoneNo;
+                                Display();
+                                break;
+                            case 8:
+                                string emailid = Console.ReadLine();
+                                contact.Email = emailid;
+                                Display();
+                                break;
+                            case 9:
+                                flag = false;
+                                break;
+                            default:
+                                Console.WriteLine("your choice should be between 1 to 9");
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        public void Display()
+        {
+            foreach (var contact in addresslist)
+            {
+                Console.WriteLine("firstname =" + contact.FirstName + "\nlastname =" + contact.LastName + "\njAddress =" + contact.Address + "\nCity =" + contact.City + "\nState =" + contact.State + "\nZip =" + contact.Zip + "\nPhone-Number =" + contact.PhNo + "Email-Id =" + contact.Email);
+            }
+        }
+        public void DeleteContact(string name)
+        {
+            CreateContact delete = new CreateContact();
+            foreach (var contact in addresslist.ToList())
+            {
+                if (contact.FirstName == name || contact.LastName == name)
+                {
+                    delete = contact;
+                }
+                addresslist.Remove(delete);
+                Console.WriteLine(name + "contact has deleted");
+            }
+        }
+        public void AddUniqueContact(string firstname)
+        {
+            foreach (var contact in addresslist)
+            {
+                if (addresslist.Contains(contact))
+                {
+                    string uniquename = Console.ReadLine();
+                    dict.Add(uniquename, addresslist);
+                }
+            }
+        }
+        public void DisplayUniqueContact()
+        {
+            Console.WriteLine("Enter the name of dictionary");
+            string firstname = Console.ReadLine();
+            foreach (var contact in dict)
+            {
+                if (contact.Key == firstname)
+                {
+                    foreach (var item in contact.Value)
+                    {
+                        Console.WriteLine("Firstname:" + item.FirstName + "\n" + "lastname:" + item.LastName + "\n" + "Address:" + item.Address + "\n" + "City:" + item.City + "\n" + "State:" + item.State + "\n" + "Zip:" + item.Zip + "\n" + "Phone.No-" + item.PhNo + "\n" + "Email.Id:" + item.Email);
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,37 +2,73 @@
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook");
-            //CreateContact contact = new CreateContact();
-            //AddContact addcontact = new AddContact();
-            //addcontact.AddNewContact();
-            //Console.WriteLine();
-            //addcontact.Display();
-            //Console.WriteLine();
-            //addcontact.EditContact();
-            //Console.WriteLine();
-            //addcontact.Display();
-            //editContact editcontact = new editContact();
-            //editcontact.AddNewContact();
-            //Console.WriteLine();
-            //editcontact.Display();
-            //Console.WriteLine();
-            //editcontact.EditContact();
-            //Console.WriteLine();
-            //editcontact.Display();
-            //deleteContact deletecontact = new deleteContact();
-            //deletecontact.AddNewContact();
-            //Console.WriteLine();
-            //deletecontact.Display();
-            //Console.WriteLine();
-            //deletecontact.RemoveContact();
-            //Console.WriteLine();
-            //deletecontact.Display();
-            AddMultipleContact addMultipleContact = new AddMultipleContact();
-            addMultipleContact.AddMultipleContacts();
-
+            Console.WriteLine("WELCOME TO ADDRESS BOOK SYSTEM");
+            bool flag = true;
+            int option;
+            CreateContact newcontact = new CreateContact();
+            AddressBook add = new AddressBook();
+            while (flag == true)
+            {
+                Console.WriteLine("CHOOSE OPTION\n1.Create Contact\n2.Add contact in addressbook\n3.Edit contact\n4.Delete Contact\n5.Add Unique Contact\n6.Exit");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("create contact in specific order:firstname,lastname,address,city,state,zip,phone number,email-id.");
+                        newcontact = new CreateContact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhNo = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        break;
+                    case 2:
+                        Console.WriteLine("Add contact in specific order:firstname,lastname,address,city,state,zip,phone.no,email-id");
+                        newcontact = new CreateContact();
+                        add.Addcontact(newcontact);
+                        newcontact.FirstName = Console.ReadLine();
+                        newcontact.LastName = Console.ReadLine();
+                        newcontact.Address = Console.ReadLine();
+                        newcontact.City = Console.ReadLine();
+                        newcontact.State = Console.ReadLine();
+                        newcontact.Zip = Console.ReadLine();
+                        newcontact.PhNo = Console.ReadLine();
+                        newcontact.Email = Console.ReadLine();
+                        add.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("enter the name of contact to be edit");
+                        string user = Console.ReadLine();
+                        add.Editcontact(user);
+                        add.Display();
+                        break;
+                    case 4:
+                        Console.WriteLine("enter the name of contact to be delete");
+                        string name = Console.ReadLine();
+                        add.DeleteContact(name);
+                        add.Display();
+                        break;
+                    case 5:
+                        Console.WriteLine("Entre the firstname as a uniquename");
+                        string firstname = Console.ReadLine();
+                        add.AddUniqueContact(firstname);
+                        add.DisplayUniqueContact();
+                        break;
+                    case 6:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your choice should be between 1 to 6");
+                        break;
+                }
+            }
         }
     }
 }
